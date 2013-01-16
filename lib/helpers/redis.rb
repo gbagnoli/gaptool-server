778b90ca5a7b9bcaa47b37efe9210b208b0fc2c2
Matt Bailey <mbailey@system42.net>
2013-01-16 12:32:08 -0800

    remote lpush

--- lib/helpers/redis.rb
+++ lib/helpers/redis.rb
@@ -12,4 +12,18 @@ module RedisHelpers
     end
     return result
   end
+
+  def remotelpush(list, value)
+    @remote_redis = Redis.new(
+      :host => $redis.hget('config', 'remoteredis:host'),
+      :port => 6379 || $redis.hget('config', 'remoteredis:port'),
+      :password => nil || $redis.hget('config', 'remoteredis:password')
+    )
+    begin
+      @remote_redis.lpush(list, value)
+      return { success => "true" }
+    rescue
+      return { success => "false" }
+    end
+  end
 end
--- lib/routes/redis.rb
+++ lib/routes/redis.rb
@@ -4,4 +4,8 @@ class GaptoolServer < Sinatra::Application
     data = JSON.parse request.body.read
     remotellen(data).to_json
   end
+  post '/status/redis/lpush' do
+    data = JSON.parse request.body.read
+    remotelpush(data['list'], data['value']).to_json
+  end
 end
